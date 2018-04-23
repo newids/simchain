52ecedd9d48b90eaf752796aa21b317b12a10469
simchain developer <simchain@iris.builders>
2018-04-23 11:59:53 +0900

    First Commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { routerTransition } from '../router.animations';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {routerTransition} from '../router.animations';
+
+import {ApiResponse} from '../api-response';
+
+import {UtilService} from '../util.service';
+import {AuthService} from '../auth.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    animations: [routerTransition()]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    constructor(public router: Router) {}
+  redirectTo: string;
+  errorResponse: ApiResponse;
+  form: FormGroup;
+  formErrors = {
+    'username': '',
+    'password': '',
+  };
+  formErrorMessages = {
+    'username': {
+      'required': 'Username is required!',
+    },
+    'password': {
+      'required': 'Password is required!',
+    },
+  };
 
-    ngOnInit() {}
+  buildForm(): void { //4
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
 
-    onLoggedin() {
-      // TODO: make login process
-        localStorage.setItem('isLoggedin', 'true');
+    this.form.valueChanges.subscribe(data => {
+      this.utilService.updateFormErrors(this.form, this.formErrors, this.formErrorMessages);
+    });
+  };
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private utilService: UtilService,
+    private authService: AuthService,
+  ) {
+    this.buildForm();
+    this.redirectTo = this.route.snapshot.queryParamMap.get('redirectTo');
+  }
+
+  ngOnInit() {
+  }
+
+  onLoggedin() {
+    // TODO: make login process
+    localStorage.setItem('isLoggedin', 'true');
+  }
+
+  submit() {
+    this.utilService.makeFormDirtyAndUpdateErrors(this.form, this.formErrors, this.formErrorMessages); //5-1
+    if (this.form.valid) {
+      this.authService.login(this.form.value.username, this.form.value.password) //5-2
+        .then(data => {
+          this.router.navigate([this.redirectTo ? this.redirectTo : '/']); //5-3
+        })
+        .catch(response => {
+          this.errorResponse = response;
+          this.utilService.handleFormSubmitError(this.errorResponse, this.form, this.formErrors); //5-4
+        });
     }
+  }
+
 }
--- src/app/util.service.ts
+++ src/app/util.service.ts
@@ -1,22 +1,67 @@
-import { environment } from '../environments/environment';
-import { Injectable } from '@angular/core';
-import { ApiResponse } from './api-response';
+import {environment} from '../environments/environment';
+import {Injectable} from '@angular/core';
+import {ApiResponse} from './api-response';
+import {FormGroup} from '@angular/forms';
 
 @Injectable()
 export class UtilService {
-  public checkSuccess(response: any): Promise<any> { // 1
-    if (response.success) {
-      return Promise.resolve(response);
-    } else {
-      return Promise.reject(response);
+  public checkSuccess(response: any): Promise<any> {
+    if (response.success) return Promise.resolve(response);
+    else return Promise.reject(response);
+  }
+
+  public handleApiError(error: any): Promise<any> {
+    if (!environment.production) console.error('An error occurred', error);
+    return Promise.reject(error);
+  }
+
+  public updateFormErrors(form: FormGroup, formErrors: any, formErrorMessages: any) { //1
+    if (!form) {
+      return;
+    }
+
+    for (const field in formErrors) {
+      formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = formErrorMessages[field];
+        if (messages) {
+          for (const key in control.errors) {
+            formErrors[field] += messages[key] + ' ';
+          }
+        }
+      }
     }
   }
 
-  public handleApiError(error: any): Promise<any> { // 2
-    if (!environment.production) {
-      console.error('An error occurred', error);
+  public makeAllFormFieldsDirty(form: FormGroup) { //2
+    if (!form) {
+      return;
     }
-    return Promise.reject(error);
+
+    for (var field in form.controls) {
+      const control = form.get(field);
+      if (control) control.markAsDirty();
+    }
+  }
+
+  public makeFormDirtyAndUpdateErrors(form: FormGroup, formErrors: any, formErrorMessages: any) { //3
+    this.makeAllFormFieldsDirty(form);
+    this.updateFormErrors(form, formErrors, formErrorMessages);
+  }
+
+  public handleFormSubmitError(response: ApiResponse, form: FormGroup, formErrors: any): void { //4
+    if (response.errors) {
+      for (const field in formErrors) {
+        const control = form.get(field);
+        if (response.errors[field] && response.errors[field].message) {
+          formErrors[field] += response.errors[field].message;
+        }
+      }
+      if (response.errors.unhandled) {
+        response.message += response.errors.unhandled;
+      }
+    }
   }
 
 }
