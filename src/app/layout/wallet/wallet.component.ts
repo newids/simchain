a301c2aa2dd845e2e4683a74211bd7adcf4b0811
newid-app-insight-com <newid-app-insight-com@users.noreply.github.com>
2018-07-11 17:26:34 +0900

    transaction / sign 함수 수정 : 서버 오류 제거 목적 import * as bitcoin from 'bitcoinjs-lib' 제거

--- src/app/layout/wallet/wallet.component.ts
+++ src/app/layout/wallet/wallet.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import * as bitcoin from 'bitcoinjs-lib';
+// import * as bitcoin from 'bitcoinjs-lib';
 import * as bigi from 'bigi';
-import {ECPair} from 'bitcoinjs-lib';
+import {ECSignature, ECPair, crypto, networks} from 'bitcoinjs-lib';
 import {KeyService} from '../../interface/key.service';
 import {Key} from '../../interface/key.interface';
 import {Subject} from 'rxjs/Subject';
@@ -43,16 +43,16 @@ export class WalletComponent implements OnInit {
 
   generateKey() {
     try {
-      const hash = bitcoin.crypto.sha256(Buffer.from(String(Math.floor(Math.random() * 100000)), 'utf-8'));
+      const hash = crypto.sha256(Buffer.from(String(Math.floor(Math.random() * 100000)), 'utf-8'));
       const x = bigi.fromBuffer(hash);
 
-      this.keyPair = new bitcoin.ECPair(x, null, {compressed: true, network: bitcoin.networks.bitcoin});
+      this.keyPair = new ECPair(x, null, {compressed: true, network: networks.bitcoin});
       this.privateKeyValue = this.keyPair.d.toHex();
       this.publicKeyValue = bigi.fromBuffer(this.keyPair.getPublicKeyBuffer()).toHex();
       this.wifCompValue = this.keyPair.toWIF();
       this.addressValue = this.keyPair.getAddress();
 
-      const keyForCompressed = new bitcoin.ECPair(x, null, {compressed: false, network: bitcoin.networks.bitcoin});
+      const keyForCompressed = new ECPair(x, null, {compressed: false, network: networks.bitcoin});
       this.wifValue = keyForCompressed.toWIF();
 
       console.log('compAddress', keyForCompressed.getAddress());
